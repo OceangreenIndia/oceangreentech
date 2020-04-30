91710c6b6776afb81f77007473e367fd32d35dbe
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 02:15:14 +0530

    enables post thumbnails for theme

--- functions.php
+++ functions.php
@@ -4,17 +4,16 @@
     function og_setup(){
         wp_enqueue_style( "bootstrap", '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
         wp_enqueue_style( "style", get_stylesheet_uri( ), null, microtime(), 'all');
-        
-        /**
-         * first parameter is name of file
-         * second for location, get_stylesheet_uri() function locate root of site, 
-         * third is dependance of file if any
-         * 4th stands for version number, for development purpose microtime() function is added to chaage dev version constanly
-         * 5th is device support, all for all device
-         */
-
          wp_enqueue_script( 'main', get_theme_file_uri( 'js/main.js' ) , null, microtime(), true );
     }
 
-    add_action( 'wp_enqueue_scripts', 'og_setup' )
+    add_action( 'wp_enqueue_scripts', 'og_setup' );
+
+    //Adding Theme Support
+
+    function og_init(){
+        add_theme_support( 'post-thumbnails');
+    }
+
+    add_action( 'after_setup_theme','og_init');
 ?>
