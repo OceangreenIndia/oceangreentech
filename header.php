50ea7f98a4edb107379b8eee43bada21adbcebd0
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 02:17:50 +0530

    enable Dynamic title instead of static title

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
         <meta charset="UTF-8">
         <meta name="Viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UI-Compatible" content="ie-edge">
-        <title>Oceangren Technology</title>
         <?php wp_head(  ) ?>
     </head>
     <body>
