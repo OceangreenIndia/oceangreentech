bfe638bc02015954ce573b6fe7174e8ef00e12f8
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 01:06:08 +0530

    wp_enqueue_style function enqueuing the stylesheet through function

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+    //Adding Css and Js file
+
+    function og_setup(){
+        wp_enqueue_style( "style",get_stylesheet_uri(  ), null, microtime(), all );
+        /**
+         * first parameter is name of file
+         * second for location, get_stylesheet_uri() function locate root of site, 
+         * third is dependance of file if any
+         * 4th stands for version number, for development purpose microtime() function is added to chaage dev version constanly
+         * 5th is device support, all for all device
+         */
+    }
+?>
