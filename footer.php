40052e3db5bd3e2497a82e8a90f72eb0cdb6460e
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 13:48:48 +0530

    adding footer. a bootstrap container class as , and row class to hold colums

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
         <footer>
-            <p>site footer</p>
+            <section id="footer">
+                <div class="container">
+                    <div class="row">
+                    
+                    </div>
+                </div>
+            </section>
         </footer>
         <?php wp_footer(  ) ?>
     </body>
