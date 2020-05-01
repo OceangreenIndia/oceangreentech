758d82913241ab6870bb8a7abde4b66e47e61757
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 13:49:44 +0530

    a single colunm is created with list group class according to bootstrap

--- footer.php
+++ footer.php
@@ -2,7 +2,15 @@
             <section id="footer">
                 <div class="container">
                     <div class="row">
-                    
+                        <div class="col-md-3">
+                            <ul class="list-group">
+                                <li class="list-group-item"><h5>Social Links</h5></li>
+                                <li class="list-group-item">Facebook</li>
+                                <li class="list-group-item">Twitter</li>
+                                <li class="list-group-item">Github</li>
+                                <li class="list-group-item">Msginc</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
