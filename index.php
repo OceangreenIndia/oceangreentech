c02931bb378b182f746f744fe35f2d3d2c0df201
Rahul Shahare <rahulshahare@gmail.com>
2020-05-02 00:19:45 +0530

    here the link is created for the post. to view full post

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 while(have_posts( )){
      the_post();
 ?>
-    <h3><?php the_title() ?></h3>
+    <h3><a href="<?php the_permalink()?>"><?php the_title() ?></a></h3>
     <p><?php the_excerpt( )?></p>
 <?php
 }
