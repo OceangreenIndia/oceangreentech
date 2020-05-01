ffabc7f290a467cb2d5d43048d7bc3022954b0b5
Rahul Shahare <rahulshahare@gmail.com>
2020-05-02 01:07:56 +0530

    adding bootstrap support

--- single.php
+++ single.php
@@ -7,13 +7,18 @@
         // the_post_thumbnail();
         // the_content();
     ?>
+    <div class="container">
     <h2><?php the_title(); ?></h2>
     <h5>Posted By<?php the_author(); ?></h5>
-    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID())?>"/>
+    </div>
+    <div class="container-fluid">
+    <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url( get_the_ID())?>"/>
+        </div>
+    <div class="container">
     <?php
     the_content();
     comment_form();
     }
 ?>
-
+    </div>
 <?php get_footer(); ?>
