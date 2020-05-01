05af2d33ba93435df985cff02b273e6204a9f941
Rahul Shahare <rahulshahare@gmail.com>
2020-05-02 00:56:37 +0530

    this is how a single post can be shown

--- single.php
+++ single.php
@@ -4,8 +4,15 @@
     //Showing requested page
     while(have_posts()){
         the_post();
-        the_post_thumbnail();
-        the_content();
+        // the_post_thumbnail();
+        // the_content();
+    ?>
+    <h2><?php the_title(); ?></h2>
+    <h5>Posted By<?php the_author(); ?></h5>
+    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID())?>"/>
+    <?php
+    the_content();
+    comment_form();
     }
 ?>
 
