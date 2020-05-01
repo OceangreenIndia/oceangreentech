f82e44c96fcdc8d3ef61945d9fdfeb1976fb7a10
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 13:53:44 +0530

    Footer is showing as we wanted

--- footer.php
+++ footer.php
@@ -2,17 +2,17 @@
             <section id="footer">
                 <div class="container">
                     <div class="row">
-                    <?php for($i=0; $i<4; $i++){?>
-                        <div class="col-md-3">
-                            <ul class="list-group">
-                                <li class="list-group-item"><h5>Social Links</h5></li>
-                                <li class="list-group-item">Facebook</li>
-                                <li class="list-group-item">Twitter</li>
-                                <li class="list-group-item">Github</li>
-                                <li class="list-group-item">Msginc</li>
-                            </ul>
-                        </div>
-                    <?php } ?>
+                        <?php for($i=0; $i<4; $i++){?>
+                            <div class="col-md-3">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h5>Social Links</h5></li>
+                                    <li class="list-group-item">Facebook</li>
+                                    <li class="list-group-item">Twitter</li>
+                                    <li class="list-group-item">Github</li>
+                                    <li class="list-group-item">Msginc</li>
+                                </ul>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
             </section>
