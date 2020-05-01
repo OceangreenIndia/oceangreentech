14f636192b593f710121bd8106a56ec97bc6fc2a
Rahul Shahare <rahulshahare@gmail.com>
2020-05-01 14:41:43 +0530

    bootstrap default class to align item in center

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
                 </div>
                 <div class="container">
                     <div class="row">
-                        <div class="col-12">
+                        <div class="col-12 text-center">
                             <h5 class="copyrightText">Copyright  <?php echo date('Y') ?> Oceangreen Technology</h5>
                         </div>
                     </div>
