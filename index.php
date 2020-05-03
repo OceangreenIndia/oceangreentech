4bcdea16342943b18cb53548a59341443cd98ad5
Rahul Shahare <rahulshahare@gmail.com>
2020-05-03 14:12:17 +0530

    index

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
 <?php
 // Getting content as per post Type
 $args = array(
-    'post_type'=>'faq',
-    'posts_per_page'=>1
+    'post_type'=>'post',
+    'posts_per_page'=>5
 );
 
 $blogpost = new WP_Query($args);
 
-while($blogpost->have_posts( )){
-    $blogpost->the_post();
+while(have_posts( )){
+    the_post();
 ?>
     <h3><a href="<?php the_permalink()?>"><?php the_title() ?></a></h3>
     <p><?php the_excerpt( )?></p>
+    <p><?php echo site_url( 'blog'); ?></p>
 <?php
 }
 ?>
